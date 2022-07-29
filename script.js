965be748420dd05589c949400614c1c9013f0452
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-07-29 18:23:22 -0500

    Picked planet

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { addDestinationInfo } = require("./scriptHelper");
 
-   let listedPlanets;
+window.addEventListener("load", function() {
+    let form=document.querySelector('form')
+
+   let listedPlanets; 
+
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
+     }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let pickedPlanet = pickPlanet(listedPlanets)
+
+       addDestinationInfo(document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.image)
    })
+   
    
 });

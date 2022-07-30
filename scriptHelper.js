91e339ee5a98a222ce9b9a0209a09fadaf0e72d2
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-07-29 20:23:06 -0500

    Working through kinks

--- scriptHelper.js
+++ scriptHelper.js
@@ -14,6 +14,18 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+  let missionTargetText = document.getElementById('MissionTarget')
+  missionTargetText.innerHTML = `
+  <h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${name}</li>
+   <li>Diameter: ${diameter}</li>
+   <li>Star: ${star}</li>
+   <li>Distance from Earth: ${distance}</li>
+   <li>Number of Moons: ${moons}</li>
+</ol>
+<img src="${imageUrl}">
+`
 }
 
 function validateInput(testInput) {
